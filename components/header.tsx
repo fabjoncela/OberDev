90f2c61eb043cdbcc972b3da19205d8d4016941c
fabjoncela <fabjoncela@gmail.com>
2025-09-25 00:17:19 +0200

    navbar style

--- components/header.tsx
+++ components/header.tsx
@@ -38,7 +38,7 @@ export function Header() {
   return (
     <>
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-16 items-center justify-between px-4 md:px-6">
+        <div className=" flex h-16 items-center justify-between px-4  xl:px-70">
           <Link href={`/${language}/`} className="flex items-center space-x-2">
             <img
               src="/MainLogo2.png"
